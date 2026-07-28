43323f93e9c8f987a42eaafb537133536362ee27
Ahmed Hussein <hsino105@gmail.com>
2026-07-28 19:57:12 +0300

    feat: add global interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './global-exception/global-exception.filter';
+import { GlobalInterceptor } from './global/global.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new GlobalInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

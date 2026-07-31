039e3e46bfbbbe99f674bc3c9a3903b51605d2b6
Ahmed Hussein <hsino105@gmail.com>
2026-07-31 20:30:19 +0300

    feat: integrate class-transformer, class-validator, cookie-parser, and helmet; configure global validation and middleware

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './global-exception/global-exception.filter';
-import { GlobalInterceptor } from './global-interceptor/global.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new GlobalExceptionFilter());
-  app.useGlobalInterceptors(new GlobalInterceptor());
+  // app.useGlobalFilters(new GlobalExceptionFilter());
+  // app.useGlobalInterceptors(new GlobalInterceptor());
+
+  // 1- Global Pipes
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  })); 
+
+
+  // 2- Global Middlewares
+  app.use(helmet());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+  });
+  // 3- Global Guards
+  // 4- Global Interceptors
+  // 5- Global Filters
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
